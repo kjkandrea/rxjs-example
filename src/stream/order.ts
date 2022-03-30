8e6db2383cc60b5e9938e8801d0f0c08322f9380
jongkeun-kim <kjkandrea@gmail.com>
2022-03-30 21:28:07 +0900

    Add combineLatest example
    언제해도 어려워

--- src/stream/order.ts
+++ src/stream/order.ts
@@ -1,8 +1,25 @@
-import { Subject } from 'rxjs'
+import { Subject, combineLatest, map } from 'rxjs'
 import {orderProducts, deliveries, deliveryCompanies} from "../infrastructure/http/order/fixture";
 
+type TypeOfDelivery = typeof deliveries[number]
+interface Delivery extends TypeOfDelivery {
+	deliveryCompanyType: 'CJ'|'DHL'
+}
+
+
 export const orderProducts$ = new Subject<typeof orderProducts>()
-export const deliveries$ = new Subject<typeof deliveries>()
+export const deliveries$ = new Subject<Delivery[]>()
 export const deliveryCompanies$ = new Subject<typeof deliveryCompanies>()
 
-orderProducts$.subscribe(console.log)
+const orderProductByDeliveryNo = combineLatest([orderProducts$, deliveries$, deliveryCompanies$]).pipe(
+	map(([orderProducts, deliveries, deliveryCompanies]) => {
+		return deliveries.map(({ shippingNo, address, deliveryCompanyType }) => ({
+			shippingNo,
+			address,
+			products: orderProducts.filter(product => product.shippingNo === shippingNo),
+			deliveryCompanyName: deliveryCompanies[deliveryCompanyType]
+		}))
+	})
+)
+
+orderProductByDeliveryNo.subscribe(console.log)
